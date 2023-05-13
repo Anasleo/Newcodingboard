161aeb6c3379b5949ac3e03a82609fc486c90045
Muhammad Anas <132725411+Anasleo@users.noreply.github.com>
2023-05-13 14:52:40 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const modal = document.getElementById("modal");
+
+const toggleModal = (value) => {
+  if (value === "open") modal.style.display = "flex";
+  if (value === "close") modal.style.display = "none";
+};
+
+
